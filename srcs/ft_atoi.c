b504d3dd7b1955a814acf502f311f919a5ffea5b
Reda96R <redarayyad@yahoo.com>
2022-08-19 13:44:00 +0100

    modified ft_atoi

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -1,5 +1,5 @@
 #include<limits.h>
-#include"libft.h"
+//#include"libft.h"
 int	ft_atoi(const char *str)
 {
 	int	i;
@@ -8,7 +8,7 @@ int	ft_atoi(const char *str)
 	r = 0;
 	s = 1;
 	i = 0;
-	while(str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+	while(str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
@@ -18,16 +18,8 @@ int	ft_atoi(const char *str)
 	}
 	while(str[i] >= '0' && str[i] <= '9')
 	{
-		if(r > INT_MAX / 10 || (r == INT_MAX / 10 && str[i] - '0' > 7))
-		{
-			if (s == 1)
-				return (-1);
-			else 
-				return (0);
-		}
-		r = (r * 10 + str[i] - '0');
+		r = r * 10 + str[i] - 48;
 			i++;
 	}
-	r *= s;
-	return (r);
+	return (r * s);
 }
